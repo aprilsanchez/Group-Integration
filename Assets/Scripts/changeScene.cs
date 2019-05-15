f8fd84c65156ab467fd68d5b4dabc6c0e7457f91
czhang19 <czhang123456@gmail.com>
2019-05-14 18:57:19 -0700

    Changed to additive loading scenes; no more 999+ errors

---
+++ Assets/Scripts/changeScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class changeScene : MonoBehaviour
+{
+    public void SceneSwitcher()
+    {
+        SceneManager.UnloadSceneAsync("scene_navigation");
+        SceneManager.LoadScene("Rattlesnake", LoadSceneMode.Additive);
+    }
+}
