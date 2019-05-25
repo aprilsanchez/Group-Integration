df786ec5b17561af50017c80db6605697ed0289f
ccatbbella <ccatbbella@gmail.com>
2019-05-25 12:20:08 -0700

    Added time

---
+++ Assets/Scripts/TimeControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TimeControl : MonoBehaviour
+{
+    System.DateTime myTime;
+    private float distance = 3f; private float yOffset = 1f;
+    float g;
+    string month, year;
+    TextMeshPro tmp;
+    float timePerRealDay; //10s in unity is 1 day in real life
+    private float timePassed = 0f; //since object first enabled
+    void Start()
+    {
+        myTime = new System.DateTime(1980, 12, 5);
+        timePerRealDay = Terrain.FindObjectOfType<Size>().growthTime;
+        tmp = GetComponent<TextMeshPro>();   
+    }
+
+    void FixedUpdate()
+    {
+        //next 3 line makes sure the time move and rotate with the camera
+        transform.LookAt(Camera.main.transform.rotation * Vector3.up);
+        transform.Rotate(new Vector3(0f,180f,0f));
+        transform.position = Camera.main.transform.position + Camera.main.transform.forward * distance + Camera.main.transform.up * yOffset;      
+
+        timePassed += Time.deltaTime;
+        if(timePassed >= timePerRealDay)
+        {
+            timePassed = timePassed % timePerRealDay;
+            myTime = myTime.AddDays(1);
+            if (myTime.Month < 10) { month = "0" + myTime.Month.ToString(); } else { month = myTime.Month.ToString(); }
+            tmp.text = myTime.Year.ToString() + " / " + month;
+        }   
+    }
+}
---
+++ Assets/Scripts/animationCurve.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class animationCurve : MonoBehaviour
+{
+    public AnimationCurve curve;
+    public float speed;
+    public float handle;
+    public List<float> biomass;
+    public ParticleSystem ps;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        ps = GetComponent<ParticleSystem>();
+        biomass = new List<float> { 1.4f, 5.6f, 2.3f, 8.5f, 5.8f, 1.6f, 4.8f };
+
+        for(int i = 0; i < biomass.Count; i++)
+        {
+            Keyframe key = new Keyframe(i, biomass[i]);
+            
+            curve.AddKey(key);
+            curve.SmoothTangents(i, 1f);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = Vector3.one * curve.Evaluate(handle);
+
+        var size = ps.sizeOverLifetime;
+        size.x = curve.Evaluate(handle);
+        size.y = curve.Evaluate(handle);
+        size.z = curve.Evaluate(handle);
+    }
+}
