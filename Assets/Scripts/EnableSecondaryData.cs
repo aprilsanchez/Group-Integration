62560453f96f7c65301b4951ec2ec0d790f16ded
ccatbbella <ccatbbella@gmail.com>
2019-05-29 22:41:19 -0700

    stufffffff

---
+++ Assets/Scripts/EnableSecondaryData.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class EnableSecondaryData : MonoBehaviour
+{
+    public int myIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void EnableTime()
+    {
+        GameObject.Find("myTime").GetComponent<TimeControl>().enabled = true;
+    }
+
+    public void EnableBothDry()
+    {
+        GameObject.Find("Biobar").GetComponent<BothDryBar>().enabled = true;
+        GameObject.Find("Prepbar").GetComponent<BothDryBar>().enabled = true;
+    }
+
+    public void EnableBothWet()
+    {
+        GameObject.Find("Biobar").GetComponent<BothWetBar>().enabled = true;
+        GameObject.Find("Prepbar").GetComponent<BothWetBar>().enabled = true;
+    }
+}
